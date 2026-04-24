b90177b2d968b164c80928787b94ab08c73763c2
Jose Maria Martin jmartin02@correo.ugr.es - Resp. Francisco Herrera Trigueros <jmartin@ngpu>
2026-04-24 18:47:02 +0200

    Update

---
+++ scripts/eval_ood.py
@@ -0,0 +1,65 @@
+import torch
+import argparse
+from openood.utils import config, setup_logger
+from openood.datasets import get_dataloader
+from openood.networks import get_network
+from openood.preprocessors import get_preprocessor
+from openood.postprocessors import get_postprocessor
+from openood.evaluators import get_evaluator
+from openood.datasets import get_ood_dataloader
+
+cfg = config.Config('/mnt/homeGPU/jmartin/TFG/configs/train.yml',
+                    '/mnt/homeGPU/jmartin/TFG/configs/eval_ood.yml')
+
+
+# -------------------------
+# Setup
+# -------------------------
+setup_logger(cfg)
+torch.manual_seed(cfg.seed)
+
+# -------------------------
+# Preprocessor
+# -------------------------
+# Para evaluación usamos ID val y OOD
+preprocessor = get_preprocessor(cfg, split='val')
+
+# -------------------------
+# Dataloaders
+# -------------------------
+dataloaders = get_dataloader(cfg)
+val_loader = dataloaders['val']  
+id_data_loaders = {'test': dataloaders['val'], 'train': dataloaders['train']}
+ood_data_loaders = get_ood_dataloader(cfg)
+# OpenOOD cargará OOD automáticamente según ood_dataset
+
+# -------------------------
+# Network
+# -------------------------
+net = get_network(cfg.network)
+net = net.cuda()
+
+# Cargar checkpoint entrenado
+if cfg.network.checkpoint is not None:
+    state = torch.load(cfg.network.checkpoint)
+    net.load_state_dict(state, strict=False)
+
+# -------------------------
+# Evaluator
+# -------------------------
+evaluator = get_evaluator(cfg)
+
+postprocessor = get_postprocessor(cfg)
+postprocessor.setup(net, id_data_loaders, ood_data_loaders)
+# -------------------------
+# Evaluación OOD
+# -------------------------
+print("Evaluando OOD con MSP...")
+ood_metrics = evaluator.eval_ood(
+    net,
+    id_data_loaders=id_data_loaders,
+    ood_data_loaders=ood_data_loaders,
+    postprocessor=postprocessor
+)
+
+print(ood_metrics)
